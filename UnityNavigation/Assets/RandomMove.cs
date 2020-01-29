26354737f3b8cbf7efae4b7b03189b65d7084d49
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2020-01-29 16:11:24 +0800

    作業修改

--- UnityNavigation/Assets/RandomMove.cs
+++ UnityNavigation/Assets/RandomMove.cs
@@ -6,12 +6,12 @@ public class RandomMove : MonoBehaviour
 {
     private NavMeshAgent agent;
 
-    
+    private Animator ani;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-       
+        ani = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,13 +19,19 @@ public class RandomMove : MonoBehaviour
         move();
     }
 
-
+  
     private void move() 
      {
         if (Input.GetKeyDown("space"))
         {
             Vector3 target = new Vector3(Random.Range(4.1f, -14.7f), 0, Random.Range(4.1f, -14.7f));
             agent.SetDestination(target);
+            ani.SetBool("跑步開關", true);
+        }
+        
+       if(agent.remainingDistance <= 0)
+        {
+            ani.SetBool("跑步開關", false);
         }
      } 
 }
