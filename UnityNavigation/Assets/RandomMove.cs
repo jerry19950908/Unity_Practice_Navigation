883bf90aefdec11b16c0219ce7aab3c8eded1ee1
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2020-01-29 15:46:00 +0800

    作業

---
+++ UnityNavigation/Assets/RandomMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomMove : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+       
+    }
+
+    private void Update()
+    {
+        move();
+    }
+
+
+    private void move() 
+     {
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 target = new Vector3(Random.Range(4.1f, -14.7f), 0, Random.Range(4.1f, -14.7f));
+            agent.SetDestination(target);
+        }
+     } 
+}
